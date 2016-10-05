4eb78b01a1c2bbde815fa0e851a08df4d147ce44
Joyce Echessa <jokhessa@yahoo.com>
2016-10-05 11:47:15 +0300

    Remove test code

--- app.js
+++ app.js
@@ -83,39 +83,37 @@ function processPostback(event) {
 }
 
 function processMessage(event) {
-    console.log("Received message from senderId: " + event.sender.id);
-    console.log("Message is: " + JSON.stringify(event.message));
-    // if (!event.message.is_echo) {
-    //     var message = event.message;
-    //     var senderId = event.sender.id;
-    //
-    //     console.log("Received message from senderId: " + senderId);
-    //     console.log("Message is: " + JSON.stringify(message));
-    //
-    //     // You may get a text or attachment but not both
-    //     if (message.text) {
-    //         var formattedMsg = message.text.toLowerCase().trim();
-    //
-    //         // If we receive a text message, check to see if it matches any special
-    //         // keywords and send back the corresponding movie detail.
-    //         // Otherwise search for new movie.
-    //         switch (formattedMsg) {
-    //             case "plot":
-    //             case "date":
-    //             case "runtime":
-    //             case "director":
-    //             case "cast":
-    //             case "rating":
-    //             getMovieDetail(senderId, formattedMsg);
-    //             break;
-    //
-    //             default:
-    //             findMovie(senderId, formattedMsg);
-    //         }
-    //     } else if (message.attachments) {
-    //         sendMessage(senderId, {text: "Sorry, I don't understand your request."});
-    //     }
-    // }
+    if (!event.message.is_echo) {
+        var message = event.message;
+        var senderId = event.sender.id;
+
+        console.log("Received message from senderId: " + senderId);
+        console.log("Message is: " + JSON.stringify(message));
+
+        // You may get a text or attachment but not both
+        if (message.text) {
+            var formattedMsg = message.text.toLowerCase().trim();
+
+            // If we receive a text message, check to see if it matches any special
+            // keywords and send back the corresponding movie detail.
+            // Otherwise search for new movie.
+            switch (formattedMsg) {
+                case "plot":
+                case "date":
+                case "runtime":
+                case "director":
+                case "cast":
+                case "rating":
+                    getMovieDetail(senderId, formattedMsg);
+                    break;
+
+                default:
+                    findMovie(senderId, formattedMsg);
+            }
+        } else if (message.attachments) {
+            sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+        }
+    }
 }
 
 function findMovie(userId, movieTitle) {
