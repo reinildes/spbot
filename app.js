cbd8caeeaac381950eb1212e9f8575e13cd5d082
Your Name <you@example.com>
2018-06-10 00:46:07 -0300

    fixing and trying out list template

--- app.js
+++ app.js
@@ -341,7 +341,7 @@ function askForMoreInfo(senderId){
             content_type:"text",
             title: "Não",
             payload: "nao",
-            image_url: serverUrl+"img?img=nao.png&time="+new Date()*1
+            image_url: serverUrl+"img?img=no.png&time="+new Date()*1
         }]};
         sendMessage(senderId, message);
     });
@@ -384,30 +384,74 @@ function askForAge(senderId){
     step = 'idade';
     sendMessage(senderId, {text: "Então vamos lá!"});
     showTypingThenSend(senderId, true, ()=>{
-        message =   {
-            attachment: {
-              type: "template",
-              payload: {
-                template_type: "list",
-                top_element_style: "compact",
-                elements: [
-                  {
-                    title: "Classic T-Shirt Collection",
-                    subtitle: "See all our colors",
-                    image_url: serverUrl+"img?img=lixo.png&time="+new Date()*1,          
-                    buttons: [
+        message =  
+            {
+                "attachment": {
+                  "type": "template",
+                  "payload": {
+                    "template_type": "list",
+                    "top_element_style": "compact",
+                    "elements": [
                       {
-                        title: "Menor que 18",
-                        type: "web_url",
-                        url: "https://peterssendreceiveapp.ngrok.io/collection",
-                        messenger_extensions: false,
-                        webview_height_ratio: "tall",
-                        fallback_url: "https://peterssendreceiveapp.ngrok.io/"            
+                        "title": "Classic T-Shirt Collection",
+                        "subtitle": "See all our colors",
+                        "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+                        "buttons": [
+                          {
+                            "title": "View",
+                            "type": "web_url",
+                            "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                            "messenger_extensions": true,
+                            "webview_height_ratio": "tall",
+                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                          }
+                        ]
+                      },
+                      {
+                        "title": "Classic White T-Shirt",
+                        "subtitle": "See all our colors",
+                        "default_action": {
+                          "type": "web_url",
+                          "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                          "messenger_extensions": false,
+                          "webview_height_ratio": "tall"
+                        }
+                      },
+                      {
+                        "title": "Classic Blue T-Shirt",
+                        "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                        "subtitle": "100% Cotton, 200% Comfortable",
+                        "default_action": {
+                          "type": "web_url",
+                          "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                          "messenger_extensions": true,
+                          "webview_height_ratio": "tall",
+                          "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                        },
+                        "buttons": [
+                          {
+                            "title": "Shop Now",
+                            "type": "web_url",
+                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                            "messenger_extensions": true,
+                            "webview_height_ratio": "tall",
+                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                          }
+                        ]        
                       }
-                    ]
-                  }]
+                    ],
+                     "buttons": [
+                      {
+                        "title": "View More",
+                        "type": "postback",
+                        "payload": "payload"            
+                      }
+                    ]  
+                  }
+                }
         }
-    }};
+
+            
         sendMessage(senderId, message);
     })
 }
